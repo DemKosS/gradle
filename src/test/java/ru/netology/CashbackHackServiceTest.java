71bd78e382cfba647de4f3c9d01b4ff01d941545
DemKosS <sidyakinkn@gmail.com>
2022-06-06 20:00:26 +0300

    change

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -2,17 +2,17 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CashbackHackServiceTest {
 
     @Test
     void remain() {
         int amount = 3000;
-        int expected =1000;
+        int expected = 1000;
 
         CashbackHackService service = new CashbackHackService();
-       int actual = service.remain(amount);
-        assertEquals( expected, actual);
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
     }
 }
