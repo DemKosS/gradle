85945aaed213afbd61872a50a02bf63707778c51
DemKosS <sidyakinkn@gmail.com>
2022-06-06 19:53:08 +0300

    initial

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain() {
+        int amount = 3000;
+        int expected =1000;
+
+        CashbackHackService service = new CashbackHackService();
+       int actual = service.remain(amount);
+        assertEquals( expected, actual);
+    }
+}
